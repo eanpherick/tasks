500b73944d4fefac93f67943a349660be970c124
Erik P. Hansen <eph@erikphansen.com>
2015-09-22 10:21:33 -0700

    show list of users in a drop down menu

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
 var app = {};
 
 $(function() { //when DOM is ready...
-	app.users = new UserCollection([
-		{username:'Person1'},
-		{username:'Person2'},
-		{username:'Person3'}
-	]);
+  app.users = new UserCollection([{
+    username: 'Kathleen'
+  },
+  {
+    username: 'Erik'
+  },
+  {
+    username: 'RZA'
+  },
+  {
+    username: 'Pilar'
+  }]);
 
-	app.tasks = new TaskCollection([
-		// test data here
-	]);
+  app.tasks = new IssueCollection([{
+    title: 'Eat Lunch',
+    description: 'it will be good',
+    creator: 'Erik',
+    assignee: 'Kathleen',
+    status: 'unassigned'
+  },
+  {
+    title: 'Do Work',
+    description: 'it will be hard',
+    creator: 'Kathleen',
+    assignee: 'Erik',
+    status: 'unassigned'
+  }
 
-	app.gui = new GUI(app.users,
-						app.tasks,
-						'#app');// selector of main div
-}
+  ]);
+
+  app.gui = new GUI(app.users, app.tasks, '#app'); // selector of main div
+});
--- views.js
+++ views.js
@@ -1,38 +1,69 @@
-var GUI = (function(){ //IIFE for all Views
+var GUI = (function() { //IIFE for all Views
+  /**
+   This view displays a single task, showing its title, description, status, creator, and assignee (if any). Each TaskView should include one or more controls (e.g. a select or set of buttons) to change its state.
 
-var TaskView = Backbone.View.extend({
+   Each task view will be associated with exactly one task model, although a model may have more than one view instance.
+   */
+  var TaskView = Backbone.View.extend({
 
-});
+  });
 
-var CreateTaskView = Backbone.View.extend({
+  /**
+   You'll need a view with input fields for the user to fill in when creating a new task. It should probably have both a create and cancel button. The location and format of the view is up to you.
+   */
+  var CreateTaskView = Backbone.View.extend({
 
-});
+  });
 
-var UnassignedTasksView = Backbone.View.extend({
+  var TaskCollectionView = Backbone.View.extend({
 
-});
+  });
 
-var UserTasksView = Backbone.View.extend({
+  // would have two TaskCollectionViews (for unassigned tasks and the current user's tasks)
+  // would also the name of the current user, a logout button, and a Create Task button
+  var UserView = Backbone.View.extend({
 
-});
+  });
 
-var UserView = Backbone.View.extend({
+  // a list of known users to choose from
+  var LoginView = Backbone.View.extend({
+    initialize: function() {
+      this.render();
+      $("#app").append(this.$el);
+    },
+    events: {
+      "click button#login": "login"
+    },
+    login: function(e) {
+      console.log("clicked:");
+      console.log(e);
+    },
+    render: function() {
+      // console.log(this.collection);
+      var usernames = this.collection.pluck("username");
+      var output = "<h1>Welcome!</h1><select><option></option>"
+      usernames.forEach(function(username) {
+        output += "<option value='" + username + "'>" + username + "</option>"
+      })
+      output += "</select><button id='login'>LOG IN</button>"
+      this.$el.html(output);
+    }
+  });
 
-});
+  // generic ctor to represent interface:
+  function GUI(users, tasks, el) {
+    // users is collection of User models
+    // tasks is collection of Task models
+    // el is selector for where GUI connects in DOM
+    //...
+    console.log("GUI");
+    this.users = users; // a UsersCollection
+    this.tasks = tasks; // an IssuesCollection
+    var loginView = new LoginView({
+      collection: this.users,
+      el: "#app"
+    });
+  }
 
-var LoginView = Backbone.View.extend({
-
-});
-
-
-// generic ctor to represent interface:
-function GUI(users,tasks,el) {
-	// users is collection of User models
-	// tasks is collection of Task models
-	// el is selector for where GUI connects in DOM
-
-	//...
-}
-
-return GUI;
-}())
+  return GUI;
+} ())
